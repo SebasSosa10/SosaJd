98eba7d66723bf96fad8fea182fa065f5ae49a14
Juan Abarka <juandiegorojasabarkaguzman@gmail.com>
2025-04-26 21:28:16 -0500

    Added Foreign Keys To Butterfly

--- fastapi/Schemes/Butterfly_Scheme.py
+++ fastapi/Schemes/Butterfly_Scheme.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel
+from typing import Optional
+from Schemes.ButterflyGarden_Scheme import ButterflyGardenResponse
 
 class ButterflyBase(BaseModel):
     species: str
     wingspan: int
     color: str
+    butterfly_garden_id: Optional[int] = None
 
 class ButterflyCreate(ButterflyBase):
     pass
 
-
 class ButterflyResponse(ButterflyBase):
     id: int
+    butterfly_garden: Optional[ButterflyGardenResponse] = None
+
     class Config:
-        orm_mode = True
+        from_attributes = True
